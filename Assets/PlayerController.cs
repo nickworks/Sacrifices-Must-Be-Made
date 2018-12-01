9eaa18efdc81a8406554f92379a67f562d7b8aa4
Nick <pattison.nick@gmail.com>
2018-12-01 01:42:19 -0500

    Added project files

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody body;
+
+    public float throttle = 100;
+    public float turnMultiplier = 100;
+
+    void Start () {
+        body = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3();
+        force.x += h * turnMultiplier;
+        force.z += v * throttle;
+        
+        body.AddForce(force * Time.deltaTime);
+        float yaw = Mathf.Atan2(body.velocity.x, body.velocity.z);
+        float pitch = -body.velocity.y / 15;
+        transform.eulerAngles = new Vector3(pitch, yaw, 0) * 180 / Mathf.PI;
+    }
+}
