2e7010d67dba8e8345bf6179408c6f9adfae0ef7
AJamesSmith <smithja@ferris.edu>
2018-12-01 19:44:51 -0400

    Almost have shooting projectiles done
    So I have a rotating turret up and running for letting the player aim around and fire. 
    I also have some shell code for the player shooting a projectile.
    Also created a projectile prefab
    
    TODO: Spawn and shoot a projectile

---
+++ Assets/TurretRotation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretRotation : MonoBehaviour {
+    Camera mainCamera;
+
+    public GameObject projectile;
+    public Transform spawnPosition;
+    Quaternion startingRotation;
+
+    public float rayLength;
+    // Use this for initialization
+    void Start () {
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            
+
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(rayLength);
+
+            if(Physics.Raycast(ray.origin, rayPoint,  out hit))
+            {
+                transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+            }
+
+            Debug.DrawLine(ray.origin, rayPoint, Color.red);
+
+            
+
+        }
+
+    }
+
+
+  
+}
