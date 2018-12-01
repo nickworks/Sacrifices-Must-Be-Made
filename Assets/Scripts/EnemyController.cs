72efb13bcd41c1711a5516169bd5d0b590997a12
Dominic <djmodd@live.com>
2018-12-01 14:20:36 -0500

    Added Basic Enemy
    + Enemy Controller Script
    + Enemy Target's the player's Z position
    + added enemy prefab

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,74 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class EnemyController : MonoBehaviour {
 
     Rigidbody body;
+    public Transform target;
     public Transform suspension;
     public Transform model;
 
-    public float throttleMin = 10;
-    public float throttleMax = 50;
+    public float throttleMin = 800;
+    public float throttleMax = 2000;
     public float turnMultiplier = 100;
+
+    /// <summary>
+    /// The target distance from the player along the z axis
+    /// </summary>
+    float offset = 10;
+    
 
     bool isGrounded = false;
 
-    void Start () {
-        body = GetComponent<Rigidbody>();	
-	}
+    void Start() {
+        body = GetComponent<Rigidbody>();
+    }
 
-	void Update () {
+    void Update() {
         RaycastHit hit;
         isGrounded = (Physics.Raycast(transform.position, Vector3.down, out hit, 1.0f));
 
         if (isGrounded) UpdateGrounded(hit);
         else UpdateAir();
-        
+
     }
 
-    void UpdateGrounded(RaycastHit hit)
-    {
+    void UpdateGrounded(RaycastHit hit) {
         Vector3 forward = Vector3.Cross(Vector3.right, hit.normal);
         float yaw = Drive(forward);
         SetModelPosAndRot(Quaternion.FromToRotation(Vector3.up, hit.normal), yaw);
     }
 
-    void UpdateAir()
-    {
+    void UpdateAir() {
         float yaw = Drive(Vector3.forward);
         float pitch = -body.velocity.y * 2;
 
         SetModelPosAndRot(Quaternion.Euler(pitch, 0, 0), yaw);
     }
 
-    float Drive(Vector3 forward)
-    {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+    
+    //cut player off?
+    //set player into tailspin?
+    //knock player of edge or into wall?
+    
 
-       // print(v);
+    float Drive(Vector3 forward) {
 
-        forward.x += h * turnMultiplier;
+        float dist = target.position.z - transform.position.z; //how far away is the player
+
+        float v = dist / offset; // divide that by the desierd offset
 
         float throttle = Mathf.Lerp(throttleMin, throttleMax, v);
-        Vector3 force = forward * throttle;
 
-        //print("player: " + force);
-        
+        Vector3 force = forward * throttle;//foreward speed
+
         Debug.DrawRay(transform.position, forward);
 
         body.AddForce(force * Time.deltaTime);
 
         float turnAngle = Mathf.Atan2(body.velocity.x, body.velocity.z) * 180 / Mathf.PI;
         return turnAngle;
+       
     }
 
-    void SetModelPosAndRot(Quaternion rot, float turn)
-    {
+    void SetModelPosAndRot(Quaternion rot, float turn) {
         float rotateSpeed = isGrounded ? 120 : 40; // the maximum number of degrees to rotate per second
 
         suspension.position = transform.position; // make the model follow the hamster wheel! ////////////////////// NOTE: If suspension is a child of the veichle do we need thi? 
         suspension.rotation = Quaternion.RotateTowards(suspension.rotation, rot, rotateSpeed * Time.deltaTime);
-        if(model) model.localEulerAngles = new Vector3(0, turn, 0);
+        if (model) model.localEulerAngles = new Vector3(0, turn, 0);
     }
 }
