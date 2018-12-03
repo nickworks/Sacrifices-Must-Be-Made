f0480421eb18688956eaff794ce6e81577b579ae
Nick <pattison.nick@gmail.com>
2018-12-02 23:18:18 -0500

    Moved scripts in scripts folder

---
+++ Assets/Scripts/PickupSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour {
+    public GameObject prefabBarrel;
+    public Transform cam;
+    public float timer;
+
+    public int spawnAmount;
+	// Use this for initialization
+	void Start () {
+        SpawnPickups(spawnAmount = Random.Range(1, 10));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= 1* Time.deltaTime;
+        if(timer <= 0)
+        {
+            SpawnPickups(spawnAmount = Random.Range(1, 10));
+            ResetTimer();
+        }
+    }
+    void ResetTimer()
+    {
+        timer = Random.Range(1, 15);
+    }
+    void SpawnPickups(int spawnAmount)
+    {
+        //print("WHAT");
+        for (int i = 0; i < spawnAmount; i ++)
+        {
+            
+            Vector3 pos = Vector3.zero;
+            pos.x += Random.Range(-20, 20);
+            pos.z = cam.position.z + Random.Range(50,100);
+            pos.y = cam.position.y;
+            GameObject.Instantiate(prefabBarrel, pos, Quaternion.identity);
+        }
+    }
+}
